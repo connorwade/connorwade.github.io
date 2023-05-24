84b77ffd06c75b376c01bd3decb4dbf024bca181
Connor Wade <connor.wade.ut@gmail.com>
2023-05-23 19:52:07 -0600

    feat: new home page style

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,8 +1,15 @@
-import { getCollection } from "astro:content";
+import { CollectionEntry, getCollection } from "astro:content";
 
 export async function getBlogs() {
   const posts = (await getCollection("blog"))
     .filter((blog) => !blog.data.draft)
     .sort((a, b) => a.data.pubDate.valueOf() - b.data.pubDate.valueOf());
+  return posts;
+}
+
+export function sortBlogs(collection: CollectionEntry<"blog">[]) {
+  const posts = collection.sort(
+    (a, b) => b.data.pubDate.getTime() - a.data.pubDate.getTime()
+  );
   return posts;
 }
