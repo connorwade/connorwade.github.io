ce7b3dc394690be454f2f91dd0df93ac08baa1cd
Connor Wade <connor.wade.ut@gmail.com>
2023-07-11 21:29:42 -0600

    fix: should be easier to read article text now

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,5 +4,6 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui"), require("@tailwindcss/typography")],
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+  themes: ["light", "dark"],
 };
