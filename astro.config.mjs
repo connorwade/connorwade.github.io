804f96934d22dfcc525a90613a7f5208f1945cb3
Connor Wade <connor.wade.ut@gmail.com>
2025-11-18 22:59:43 -0700

    feat: marry apline, kill old state manager, fuck the progress bar

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,14 @@ import sitemap from "@astrojs/sitemap";
 
 import tailwindcss from "@tailwindcss/vite";
 
+import svelte from "@astrojs/svelte";
+
+import alpinejs from "@astrojs/alpinejs";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.codingakita.com",
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), svelte(), alpinejs()],
 
   vite: {
     plugins: [tailwindcss()],
